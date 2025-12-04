d17f740e94d301dde1463cf08b318b4b22d2985c
QUEEN GLIEZEL M. ROCHE <QGMROCHE@Complab2.vsuisabel>
2025-12-04 11:49:05 +0800

    Updated content section

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
 function changeMessage() {
-    document.getElementById("messageArea").innerText =
-        "You clicked the button! JS is working correctly.";
+
+    const messageArea = document.getElementById("messageArea");
+    messageArea.innerText = "You clicked the button! JS is working correctly.";
+    messageArea.style.color = "green";
+    messageArea.style.fontWeight = "bold";
+
+    const mainHeading = document.querySelector("header h1");
+    mainHeading.innerText = "Welcome to My Updated Website!";
+    mainHeading.style.color = "#ff6600";
+
+    const heroImage = document.querySelector(".hero img");
+    heroImage.src = "images/cat.jpg";
+    heroImage.alt = "Updated banner image";
+
+    const heroTagline = document.querySelector(".hero h2");
+    heroTagline.innerText = "Your Tagline Has Been Updated!";
 }
